e75e39a3cf9cfa8cb627b1896764ee64147508a6
Amanksingh16 <amansingh16597@gmail.com>
2018-01-23 18:05:08 -0800

    Basic of Parent Class

---
+++ Animal_OOPS.java
@@ -0,0 +1,14 @@
+
+public class Animal_OOPS 
+{
+   String Breed , colour ;
+   
+   public Animal_OOPS() {
+	   System.out.println(" This is a Animal ");
+   }
+   
+   public void Speak()
+   {
+	   System.out.println(" Growd ! ");
+   }
+}
