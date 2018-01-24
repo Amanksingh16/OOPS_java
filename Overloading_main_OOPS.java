23c1d0112d302206e3223f86a79ac3d367861e3f
Amanksingh16 <amansingh16597@gmail.com>
2018-01-23 18:08:34 -0800

    main run success with 3 functions call

---
+++ Overloading_main_OOPS.java
@@ -0,0 +1,13 @@
+
+public class Overloading_main_OOPS {
+
+	public static void main(String[] args)
+	{
+		Overloading_OOPS Obj = new Overloading_OOPS();
+		Obj.print();
+		Obj.print(10);
+		Obj.print(4.5f);
+
+	}
+
+}
