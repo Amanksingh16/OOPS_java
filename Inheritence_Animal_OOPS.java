a6cf2fd6958b926ceacad35e2555f0d4d3644510
Amanksingh16 <amansingh16597@gmail.com>
2018-01-23 18:06:24 -0800

    Main Class Success

---
+++ Inheritence_Animal_OOPS.java
@@ -0,0 +1,13 @@
+
+public class Inheritence_Animal_OOPS {
+
+	public static void main(String[] args)
+	{
+		Animal_OOPS A1 = new Dog_OOPS();		
+		A1.Speak();
+		System.out.println();
+		Animal_OOPS A2 = new Cat_OOPS();
+		A2.Speak();
+	}
+
+}
